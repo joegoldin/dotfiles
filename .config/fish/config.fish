30197d91985c995be2df4cc5db22a06a597bad10
Joe Goldin <joe@joegold.in>
2022-12-08 21:47:09 -0800

    starship preset bracketed-segments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
-if test -e /opt/homebrew/bin; then fish_add_path /opt/homebrew/bin; fi
-if test -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin"; then fish_add_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"; fi
+if test -e /opt/homebrew/bin; then fish_add_path /opt/homebrew/bin; end
+if test -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin"; then fish_add_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"; end
 
-if test -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin"; then set -Ux EDITOR subl; fi
+if test -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin"; then set -Ux EDITOR subl; end
 set -Ux Z_CMD "j"
 
 starship init fish | source
