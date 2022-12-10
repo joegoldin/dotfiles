48335b5470cd119898102b451e0a552c3e328189
Joe Goldin <jgoldin@skillz.com>
2022-12-09 21:41:26 -0800

    Add more fish stuffs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,32 @@
-if test -e /opt/homebrew/bin
-    fish_add_path /opt/homebrew/bin
-end
+if status is-interactive
+    abbr -a ls "exa -la -s modified --header"
+    abbr -a awslogin "aws sso login"
 
-if test -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-    fish_add_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+    function mkdir; command mkdir -p $argv; end
+    function rm; command rm -i $argv; end
+    function cp; command cp -i $argv; end
+    function mv; command mv -i $argv; end
+    function k; command kubectl $argv; end
+    function gcm; command npx commitgpt -c $argv; end
 end
-
-if test -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-    set -Ux EDITOR subl
-end
-
-set -Ux Z_CMD "j"
 
 function fish_greeting
     echo "🐟"
 end
 
+# PATH
+if test -e /opt/homebrew/bin
+    fish_add_path /opt/homebrew/bin
+end
+if test -e "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+    fish_add_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
+    set -Ux EDITOR subl
+end
+
+# ENV
+set -Ux LANG en_US.UTF-8
+set -Ux nvm_default_version lts
+set -Ux Z_CMD "j"
+
+# Starship
 if which starship &> /dev/null; starship init fish | source; end
