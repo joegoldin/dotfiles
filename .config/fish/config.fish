7b6288850fb6db1ad3000bc4664cb06d5459ec03
Joe Goldin <joe@joegold.in>
2022-12-11 12:27:06 -0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,7 +39,7 @@ if command -v thefuck &> /dev/null; thefuck --alias | source; end
 if command -v starship &> /dev/null; starship init fish | source; end
 
 # Fisher plugins
-if test -e ~/.config/fish/.fisherinstalled
+if ! test -e ~/.config/fish/.fisherinstalled
     curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
     if test $status -eq 0
         touch ~/.config/fish/.fisherinstalled
