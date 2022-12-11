72de20e293adbf905ce395386742afde7a3867de
Joe Goldin <joe@joegold.in>
2022-12-11 10:05:37 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,8 +15,8 @@ function link_files() {
 
 # INSTALLER FUNCTIONS
 function install_exa() {
-    set -x EXA_VERSION (curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-    curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v$EXA_VERSION.zip"
+    EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
     sudo unzip -q exa.zip bin/exa -d /usr/local
     rm -rf exa.zip
 }
