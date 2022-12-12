27c83e9d9a861a1732baea879de5a46969dcd6f9
jgoldin <joe@joegold.in>
2022-12-12 13:51:16 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,27 +36,6 @@ function install_haxe
     mkdir ~/.haxelib_home && haxelib setup ~/.haxelib_home
 end
 
-function install_gotools
-    # install gopls, dlv, hey
-    go get -u golang.org/x/tools/gopls
-    go get -u github.com/go-delve/delve/cmd/dlv
-    go get -u github.com/rakyll/hey
-    # vscode-go dependencies 
-    # via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
-    go get -u -v github.com/acroca/go-symbols
-    go get -u -v github.com/cweill/gotests/...
-    go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-    go get -u -v github.com/haya14busa/goplay/cmd/goplay
-    go get -u -v github.com/mdempsky/gocode
-    go get -u -v github.com/ramya-rao-a/go-outline
-    go get -u -v github.com/rogpeppe/godef
-    go get -u -v github.com/sqs/goreturns
-    go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
-    go get -u -v github.com/zmb3/gogetdoc
-    go get -u -v golang.org/x/lint/golint
-    go get -u -v golang.org/x/tools/cmd/gorename
-end
-
 # INSTALL SOFTWARE
 function install_software
     sleep 20
@@ -70,7 +49,6 @@ function install_software
     cargo install wasm-server-runner
     cargo install cargo-watch
     cargo install matchbox_server
-    
     install_exa
     npm install -g http-server webpack webpack-cli typescript ts-loader
     echo (date +"%Y-%m-%d %T")
