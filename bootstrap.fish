ff33dddd7cc5f3a4f9f832cd977311f3dd37d836
Joe Goldin <joe@joegold.in>
2023-07-22 15:36:08 -0700

    Update bootstrap.fish

--- bootstrap.fish
+++ bootstrap.fish
@@ -66,6 +66,8 @@ function install_common_packages
     yes | pip3 install thefuck --upgrade
     yes | npm install -g http-server webpack webpack-cli typescript ts-loader @githubnext/github-copilot-cli simple-https-proxy@latest
     simple-https-proxy --makeCerts=true
+    wget -qO- "https://getbin.io/suyashkumar/ssl-proxy" | tar xvz 
+    sudo mv ssl-proxy* /usr/local/bin/ssl-proxy
     set -x EXA_VERSION (curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
     curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v$EXA_VERSION.zip"
     sudo unzip -q exa.zip bin/exa -d /usr/local
