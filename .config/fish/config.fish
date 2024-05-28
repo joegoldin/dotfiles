3dce3910eb96f1440ad896c1ac6f9148c7076a29
Joe Goldin <joe@joegold.in>
2024-05-28 12:27:29 -0700

    chore: cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -54,38 +54,6 @@ if ! test -e ~/.config/fish/.fisherinstalled
     end
 end
 
-# zellij aliases
-alias ze='zellij'
-
-# zellij attach [container]
-function za
-    zellij attach "$argv"
-end
-
-# zellij run [command]
-function zr
-    zellij run --name "$argv" -- fish -c "$argv"
-end
-
-# zellij run floating [command]
-function zrf
-    zellij run --name "$argv" --floating -- fish -c "$argv"
-end
-
-# zellij edit file [file]
-function zed
-    zellij edit $argv
-end
-
-# zellij attach
-function zs
-    set sessions (zellij list-sessions --no-formatting | awk '{printf "\033[1;36m%-20s\033[0m %s\n", $1, $3}')
-    set selected_session (echo "$sessions" | fzf --height (set -q FZF_TMUX_HEIGHT; and echo $FZF_TMUX_HEIGHT; or echo 20%) --ansi)
-    if test -n "$selected_session"
-        za (echo $selected_session | awk '{print $1}')
-    end
-end
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f ~/miniconda3/bin/conda
