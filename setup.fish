486c5585ea10d5fc4b8f54a34d98c3afa5b67aef
Joe Goldin <joe@joegold.in>
2024-05-28 13:41:41 -0700

    fix: splitup mac and linux install stuff

--- setup.fish
+++ setup.fish
@@ -20,8 +20,90 @@ function log
     echo $logMessage >> $log_file
 end
 
+function brew_upgrade
+    log '⚙️ Updating Homebrew...'
+    if test (brew -v)
+        # Update Homebrew
+        brew update
+    else
+        # Install Homebrew
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        log '✔️ Homebrew installed successfully -- please re-run this script.'
+        exit 0
+    end
+    log '✔️ Homebrew updated successfully.'
+end
+
+function apt_upgrade
+    log '⚙️ Upgrading packages...'
+    set -x DEBIAN_FRONTEND noninteractive
+    sudo chmod -R 1777 /tmp
+    sudo apt update
+    DEBIAN_FRONTEND=noninteractive sudo apt install -y debconf-utils
+    sudo debconf-set-selections < .dpkg-selections.conf
+    sudo apt-add-repository ppa:fish-shell/release-3 --yes
+    sudo apt update --yes
+    sudo apt install -y fish
+    sudo chsh -s /usr/bin/fish (whoami)
+    log '✔️ Packages upgraded successfully.'
+end
+
+function install_common_packages
+    log '💽 Installing common packages...'
+    yes | pip3 install thefuck --upgrade
+    yes | npm install -g http-server webpack webpack-cli typescript ts-loader simple-https-proxy@latest
+    simple-https-proxy --makeCerts=true
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    cargo install --locked zellij
+    go install github.com/baalimago/clai@latest
+    log '✔️ Common packages installed successfully.'
+end
+
+function install_software_linux
+    log '💽 Installing linux software...'
+    sudo apt install dpkg -y
+    sudo apt update
+    sudo apt -o DPkg::Lock::Timeout=800 install golang unzip libgl1-mesa-glx mesa-utils xauth build-essential \
+        kitty-terminfo socat ncat bat jq ripgrep thefuck tmux libfuse2 fuse software-properties-common libpng-dev \
+        libturbojpeg-dev libvorbis-dev libopenal-dev libsdl2-dev libmbedtls-dev libuv1-dev libsqlite3-dev libncurses-dev \
+        automake autoconf xsltproc fop dialog mosh -y
+    wget -qO- "https://getbin.io/suyashkumar/ssl-proxy" | tar xvz 
+    sudo mv ssl-proxy* /usr/local/bin/ssl-proxy
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+    fish_add_path /usr/local/go/bin
+    curl -sL https://cdn.geekbench.com/Geekbench-6.1.0-Linux.tar.gz | tar xvz && sudo mv Geekbench*/geekbench* /usr/local/bin/. && rm -rf Geekbench*
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $home_dir/miniconda.sh
+    bash $home_dir/miniconda.sh -b -p $home_dir/miniconda
+    log '✔️ Linux software installed successfully.'
+end
+
+function install_software_mac
+    log '💽 Installing mac software...'
+    brew install fish go 
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+    bash ~/miniconda.sh -b -p $home_dir/miniconda
+    log '✔️ Mac software installed successfully.'
+end
+
+function install_software
+    sleep 5
+
+    switch (uname -s)
+    case "*Darwin*"
+        brew_upgrade
+        install_software_mac
+        install_common_packages
+    case "*Linux*"
+        apt_upgrade
+        install_software_linux
+        install_common_packages
+        sudo chmod -R 1777 /tmp
+        sudo rm -rf /tmp/*
+    end
+end
+
 function link_files
-    log '🔗 Linking files.'
+    log '🔗 Linking files...'
     mkdir -p $home_dir/.config
     # ssh
     mkdir -p $home_dir/.ssh
@@ -45,73 +127,23 @@ function link_files
     # zellij
     rm -rf $home_dir/.config/zellij
     mkdir -p $home_dir/.config/zellij
+    ln -s (pwd)/.config/zellij/config.kdl $home_dir/.config/zellij/config.kdl
     ln -s (pwd)/.config/zellij/plugins $home_dir/.config/zellij/plugins
     log '✔️ Files linked successfully.'
 end
 
-function apt_upgrade
-    log '⚙️ Upgrading packages.'
-    set -x DEBIAN_FRONTEND noninteractive
-    sudo chmod -R 1777 /tmp
-    sudo apt update
-    DEBIAN_FRONTEND=noninteractive sudo apt install -y debconf-utils
-    sudo debconf-set-selections < .dpkg-selections.conf
-    sudo apt-add-repository ppa:fish-shell/release-3 --yes
-    sudo apt update --yes
-    sudo apt install -y fish
-    sudo chsh -s /usr/bin/fish codespace
-    log '✔️ Packages upgraded successfully.'
-end
-
-function install_common_packages
-    sudo apt install dpkg -y
-    sudo apt update
-    sudo apt -o DPkg::Lock::Timeout=800 install golang unzip libgl1-mesa-glx mesa-utils xauth build-essential \
-        kitty-terminfo socat ncat bat jq ripgrep thefuck tmux libfuse2 fuse software-properties-common libpng-dev \
-        libturbojpeg-dev libvorbis-dev libopenal-dev libsdl2-dev libmbedtls-dev libuv1-dev libsqlite3-dev libncurses-dev \
-        automake autoconf xsltproc fop dialog mosh -y
-    yes | pip3 install thefuck --upgrade
-    yes | npm install -g http-server webpack webpack-cli typescript ts-loader @githubnext/github-copilot-cli simple-https-proxy@latest
-    simple-https-proxy --makeCerts=true
-    wget -qO- "https://getbin.io/suyashkumar/ssl-proxy" | tar xvz 
-    sudo mv ssl-proxy* /usr/local/bin/ssl-proxy
-    curl https://sh.rustup.rs -sSf | sh -s -- -y
-    cargo install --locked zellij
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
-    fish_add_path /usr/local/go/bin
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $home_dir/miniconda.sh
-    bash $home_dir/miniconda.sh -b -p $home_dir/miniconda
-    go install github.com/baalimago/clai@latest
-    curl -sL https://cdn.geekbench.com/Geekbench-6.1.0-Linux.tar.gz | tar xvz && sudo mv Geekbench*/geekbench* /usr/local/bin/. && rm -rf Geekbench*
-    set -x EXA_VERSION (curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-    curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v$EXA_VERSION.zip"
-    sudo unzip -q exa.zip bin/exa -d /usr/local
-    rm -rf exa.zip
-    curl https://raw.githubusercontent.com/ibigio/shell-ai/main/install.sh | bash
-end
-
-function install_software
-    log '💽 Installing common packages.'
-    sleep 5
-    install_common_packages
-    sudo chmod -R 1777 /tmp
-    sudo rm -rf /tmp/*
-    log '✔️ Common packages installed successfully.'
-end
-
 function setup_software
-    log '🔧 Configuring software.'
+    log '🔧 Configuring software...'
     mkdir -p $home_dir/.config/github-copilot
-    touch /home/codespace/.config/fish/.fisherinstalled
-    rm -rf /home/codespace/.config/fish/.fisherinstalled
+    touch $home_dir/.config/fish/.fisherinstalled
+    rm -rf $home_dir/.config/fish/.fisherinstalled
     echo '{"joegoldin":{"version":"2021-10-14"}}' > $home_dir/.config/github-copilot/terms.json
     log '✔️ Software configured successfully.'
 end
 
 # Run script
 log '==========STARTING INSTALLATION==========='
-link_files
-apt_upgrade
 install_software
+link_files
 setup_software
 log '==========INSTALLATION COMPLETE==========='
