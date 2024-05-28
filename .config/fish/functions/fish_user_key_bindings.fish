38f0db11234b60d4124a6d123448375630566b73
Joe Goldin <joe@joegold.in>
2024-05-28 12:25:49 -0700

    chore: cleanup and updates

---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,18 @@
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
