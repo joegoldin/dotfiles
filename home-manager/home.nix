ab2910d2b22f5e72daf15f2fdd7effe654f2f64e
joegoldin <joe@joegold.in>
2024-06-08 02:11:46 +0000

    1password

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,10 +22,20 @@
     ./core.nix
     ./git.nix
     ./starship.nix
+
+    inputs._1password-shell-plugins.hmModules.default
   ];
 
   # Enable home-manager
   programs.home-manager.enable = true;
+  
+  # programs._1password-shell-plugins = {
+  #   # enable 1Password shell plugins for bash, zsh, and fish shell
+  #   enable = true;
+  #   # the specified packages as well as 1Password CLI will be
+  #   # automatically installed and configured to use shell plugins
+  #   plugins = with pkgs; [ gh awscli2 ];
+  # };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
