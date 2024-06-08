0394e6c899e203a790ac775c0dfec130870b1b77
joegoldin <joe@joegold.in>
2024-06-08 18:51:12 +0000

    multi env fixes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,24 +19,14 @@
     # inputs.nix-colors.homeManagerModules.default
 
     # You can also split up your configuration and import pieces of it here:
-    ./shell.nix
-    ./core.nix
-    ./git.nix
-    ./starship.nix
-
-    # inputs._1password-shell-plugins.hmModules.default
+    ./modules/shell.nix
+    ./modules/core.nix
+    ./modules/git.nix
+    ./modules/starship.nix
   ];
 
   # Enable home-manager
   programs.home-manager.enable = true;
-  
-  # programs._1password-shell-plugins = {
-  #   # enable 1Password shell plugins for bash, zsh, and fish shell
-  #   enable = true;
-  #   # the specified packages as well as 1Password CLI will be
-  #   # automatically installed and configured to use shell plugins
-  #   plugins = with pkgs; [ gh awscli2 ];
-  # };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
