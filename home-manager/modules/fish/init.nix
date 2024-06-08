c3ce2c60ab9356fb9eef33b3a06de46fb5fb6740
joegoldin <joe@joegold.in>
2024-06-08 18:51:12 +0000

    multi env fixes

--- home-manager/modules/fish/init.nix
+++ home-manager/modules/fish/init.nix
@@ -4,7 +4,6 @@
   interactiveShellInit = ''
     set -Ux Z_CMD "j"
     set -Ux nvm_default_version lts
-    set -Ux SSH_AUTH_SOCK ~/.1password/agent.sock
     ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
   '';
 }
