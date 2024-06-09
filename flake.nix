90e432f51e4afc0a6b8f1eaa91d238c825ecb561
joegoldin <joe@joegold.in>
2024-06-09 20:57:47 +0000

    really actually trust

--- flake.nix
+++ flake.nix
@@ -30,6 +30,8 @@
   };
 
   nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
     experimental-features = "nix-command flakes";
   };
 
