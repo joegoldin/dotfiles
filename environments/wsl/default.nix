b3297285190f7e8bf0346bc6febfcf8db5fffdab
joegoldin <joe@joegold.in>
2024-06-09 01:31:48 +0000

    cleanup modules, share in default.nix

---
+++ environments/wsl/default.nix
@@ -0,0 +1,12 @@
+# common/default.nix
+{ lib, inputs, outputs, pkgs, config, username, hostname, stateVersion, ... }:
+
+{
+  imports = [
+    ../nixos/configuration.nix
+    ./hardware-configuration.nix
+
+    inputs.nixos-wsl.nixosModules.default
+    ./wsl.nix
+  ];
+}
