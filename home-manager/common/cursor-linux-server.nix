b4cd15cd5f3d3dd32a7193de66a7c9b4044dffee
joegoldin <joe@joegold.in>
2024-06-09 01:43:21 +0000

    more organizing and cleanup

--- home-manager/common/cursor-linux-server.nix
+++ home-manager/common/cursor-linux-server.nix
@@ -1,26 +1,6 @@
+{ lib, pkgs, ...}:
+
 {
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  username,
-  homeDirectory,
-  stateVersion,
-  ...
-}: {
-  imports = [
-    ./home.nix
-  ];
-
-  programs.git.extraConfig.gpg.ssh.program = "op-ssh-sign-wsl";
-  programs.git.extraConfig.core.sshCommand = "ssh.exe";
-  home.shellAliases = {
-    ssh = "ssh.exe";
-    ssh-add = "ssh-add.exe";
-    op = "op.exe";
-  };
-
   home.packages = with pkgs; [
     cursor-server-linux
   ];
