1581b28f09f392b32a149409cc8156acfaeb6568
joegoldin <joe@joegold.in>
2024-06-14 01:59:08 +0000

    add minimal devenv.nix example

---
+++ devenv.nix
@@ -0,0 +1,34 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+# Minimal devenv.nix
+{
+  # https://devenv.sh/reference/options/
+  cachix.enable = false;
+  dotenv.enable = true;
+
+  packages = with pkgs;
+    [
+      hello
+      fish
+      # example packages
+      # go
+      # (writeShellScriptBin "google-chrome" ''
+      #   exec -a $0 ${google-chrome}/bin/google-chrome-stable $@
+      # '')
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      # Linux specific packages
+    ]
+    ++ lib.optionals pkgs.stdenv.isDarwin [
+      # macOS specific packages
+    ];
+
+  enterShell = ''
+    # hello
+  '';
+}
