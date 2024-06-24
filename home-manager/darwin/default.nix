b21bae6de2a9c7d20d50d2a17104fe1224123933
joegoldin <joe@joegold.in>
2024-06-24 01:40:53 +0000

    misc. add barrier to macos

--- home-manager/darwin/default.nix
+++ home-manager/darwin/default.nix
@@ -19,7 +19,18 @@
     (import ../common/packages.nix {inherit pkgs;}).home.packages
     ++ (with pkgs; [
       shopt-script
+      # brewCasks.1password-cli
+      # brewCasks.stats
+      # brewCasks.android-platform-tools
+      # brewCasks.flameshot
+      # brewCasks.michaelvillar-timer
+      # brewCasks.modern-csv
+      # brewCasks.ngrok
+      # brewCasks.sanesidebuttons
+      # brewCasks.tomatobar
     ]);
 
   programs.git.extraConfig.gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+
+  services.barrier.client.enable = true;
 }
