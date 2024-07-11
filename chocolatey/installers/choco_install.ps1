5ecd334f21a6350b9aa6dc7da76e17d1526ef875
joegoldin <joe@joegold.in>
2024-07-11 18:26:42 +0000

    feat: windows setup scripts via boxstarter/chocolatey

---
+++ chocolatey/installers/choco_install.ps1
@@ -0,0 +1,32 @@
+Update-ExecutionPolicy Unrestricted
+
+choco install boxstarter
+
+choco install 7zip
+choco install barrier
+choco install clipjump
+# choco install cursoride # manually installing because of hash mismatch
+choco install discord.install
+# choco install displayfusion --pre # manually installing for beta version
+# choco install displaylink # manually installing for latest version
+choco install docker-desktop
+choco install epicgameslauncher
+# choco install figma # manually installing because of hash mismatch
+choco install jdownloader
+choco install lghub
+choco install librehardwaremonitor
+choco install localsend
+choco install logseq
+choco install microsoft-windows-terminal
+choco install mpc-hc-clsid2
+choco install msedgeredirect
+choco install notion
+choco install powertoys
+choco install quicklook
+choco install slack
+# choco install steam # manually installing because of hash mismatch
+choco install streamdeck
+choco install tailscale
+choco install wavelink
+choco install wiztree
+choco install zoom
