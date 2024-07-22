faf26f9414ca83f932478a0bc4569ba6cab70fef
joegoldin <joe@joegold.in>
2024-07-22 05:00:10 +0000

    mkForce aarch64

--- environments/oracle-cloud/oracle-cloud.nix
+++ environments/oracle-cloud/oracle-cloud.nix
@@ -5,7 +5,7 @@
   pkgs,
   ...
 }: {
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  nixpkgs.hostPlatform = lib.mkForce "aarch64-linux";
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
   networking.domain = "subnet02180136.vcn02180136.oraclevcn.com";
