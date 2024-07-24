b4917befbb2ae4cacbf721294f6664b1533aa697
joegoldin <joe@joegold.in>
2024-07-24 13:43:43 -0700

    add sf-symbols in brew

--- environments/oracle-cloud/hardware-configuration.nix
+++ environments/oracle-cloud/hardware-configuration.nix
@@ -1,6 +1,5 @@
-{ modulesPath, ... }:
-{
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+{modulesPath, ...}: {
+  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
   boot = {
     tmp = {
       useTmpfs = true;
@@ -13,9 +12,15 @@
       device = "nodev";
     };
   };
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/6536-F6CE"; fsType = "vfat"; };
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
-  boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/6536-F6CE";
+    fsType = "vfat";
+  };
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
+  boot.initrd.kernelModules = ["nvme"];
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
   zramSwap.enable = true;
 }
