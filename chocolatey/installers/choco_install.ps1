b96133b215695d6ef4cff5ebf07751567d35914b
joegoldin <joe@joegold.in>
2024-07-27 16:24:42 +0000

    add gog galaxy windows

--- chocolatey/installers/choco_install.ps1
+++ chocolatey/installers/choco_install.ps1
@@ -13,6 +13,7 @@ choco install discord.install
 choco install docker-desktop
 choco install epicgameslauncher
 # choco install figma # manually installing because of hash mismatch
+choco install goggalaxy
 choco install httpie
 choco install httpie-desktop
 choco install jdownloader
