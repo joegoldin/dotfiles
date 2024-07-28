61712043cc34bf31ce40328d8f04d2299757fa00
joegoldin <joe@joegold.in>
2024-07-28 04:51:24 +0000

    fix

--- environments/oracle-cloud/oracle-cloud.nix
+++ environments/oracle-cloud/oracle-cloud.nix
@@ -29,6 +29,7 @@
         default = "http_status:404";
       };
     };
+  };
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
