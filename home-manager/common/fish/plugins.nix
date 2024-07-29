86f8c5c84393f1fb406f7abb83662c27f865a9a7
joegoldin <joe@joegold.in>
2024-07-29 17:12:10 +0000

    add nix.fish fish plugin

--- home-manager/common/fish/plugins.nix
+++ home-manager/common/fish/plugins.nix
@@ -36,5 +36,14 @@
       name = "grc";
       src = grc.src;
     } # garabik/grc/grc.fish
+    {
+      name = "nix.fish";
+      src = pkgs.fetchFromGitHub {
+        owner = "kidonng";
+        repo = "nix.fish";
+        rev = "ad57d970841ae4a24521b5b1a68121cf385ba71e";
+        sha256 = "13x3bfif906nszf4mgsqxfshnjcn6qm4qw1gv7nw89wi4cdp9i8q";
+      };
+    }
   ];
 }
