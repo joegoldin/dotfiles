beeb33c26768e8f4f10d0447672000710cd3e149
joegoldin <joe@joegold.in>
2024-07-30 06:41:26 +0000

    enable tailscale service in oracle cloud

--- environments/oracle-cloud/oracle-cloud.nix
+++ environments/oracle-cloud/oracle-cloud.nix
@@ -30,6 +30,8 @@
 
   programs.ssh.startAgent = true;
 
+  services.tailscale.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
