992134622cb8d5229b8530f5dd07900ed39527c8
joegoldin <joe@joegold.in>
2024-08-08 00:20:25 +0000

    add docker to oracle cloud

--- environments/oracle-cloud/oracle-cloud.nix
+++ environments/oracle-cloud/oracle-cloud.nix
@@ -33,6 +33,9 @@
   services.tailscale.enable = true;
   services.tailscale.useRoutingFeatures = "server";
 
+  virtualisation.docker.enable = true;
+  users.extraGroups.docker.members = [ "joe" ];
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
