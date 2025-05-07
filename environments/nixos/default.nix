75959da5b0cd778847e44ea089a29f49bdb65469
joegoldin <joe@joegold.in>
2025-05-06 21:23:49 -0700

    wallpaper stuff

--- environments/nixos/default.nix
+++ environments/nixos/default.nix
@@ -14,5 +14,19 @@
     ./configuration.nix
     ./hardware-configuration.nix
     ./joe-desktop.nix
+    ./wallpaper.nix
   ];
+
+  services.wallpaper = {
+    enable = true;
+    wallpaperDir = "${config.users.users.${username}.home}/Pictures/Wallpaper";
+    monitorMapping = {
+      "DP-3" = 0;
+      "DP-2" = 2;
+      "HDMI-A-1" = 3;
+      "DP-1" = 1;
+      "DVI-I-2" = 4;
+      "DVI-I-1" = 5;
+    };
+  };
 }
