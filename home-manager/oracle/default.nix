820c629a163f977d481783662adddbe900a40097
joegoldin <joe@joegold.in>
2025-05-10 12:36:45 -0700

    start adding wine via mkWindowsApp from emmanuelrosa, add berkely mono fonts

---
+++ home-manager/oracle/default.nix
@@ -0,0 +1,29 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  username,
+  homeDirectory,
+  stateVersion,
+  ...
+}: {
+  imports = [
+    ../common
+    ../common/cursor-server-linux.nix
+  ];
+
+  # lorri for nix-shell
+  services.lorri.enable = true;
+
+  # gnupg gpg stuff
+  services.gnome-keyring.enable = true;
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+  };
+}
