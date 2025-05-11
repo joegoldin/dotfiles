c4da53a0580d881665df1813623cf67b9c6c240f
joegoldin <joe@joegold.in>
2025-05-10 18:52:18 -0700

    fix config, lint/format

---
+++ environments/oracle-cloud/configuration.nix
@@ -0,0 +1,109 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  username,
+  hostname,
+  stateVersion,
+  agenix,
+  ...
+}: {
+  system.stateVersion = "${stateVersion}";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    config = {
+      allowUnfree = true;
+      allowUnsupportedSystem = true;
+      experimental-features = "nix-command flakes";
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      nix-path = config.nix.nixPath;
+      trusted-users = ["${username}"];
+      auto-optimise-store = false;
+      extra-substituters = ["https://nixpkgs-python.cachix.org"];
+      extra-trusted-public-keys = ["nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=" "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
+    };
+
+    gc = {
+      automatic = lib.mkDefault true;
+      options = lib.mkDefault "--delete-older-than 7d";
+    };
+
+    extraOptions = lib.optionalString (config.nix.package == pkgs.nixVersions.stable) "experimental-features = nix-command flakes";
+
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  networking.hostName = hostname;
+
+  users.users = {
+    "${username}" = {
+      uid = 1000;
+      shell = pkgs.fish;
+      # You can set an initial password for your user.
+      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+      # Be sure to change it (using passwd) after rebooting!
+      # hashedPassword = "";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0vgzxNgZd51jZ3K/s64jltFRSyVLxjLPWM4Q6747Zw"
+      ];
+      extraGroups = ["wheel" "audio" "video" "docker" "networkmanager"];
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+  };
+
+  programs.fish = {
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    agenix.packages.${pkgs.system}.default
+    git
+    unstable.sbctl
+    wget
+  ];
+
+  # This setups a SSH server. Very important if you're setting up a headless system.
+  # Feel free to remove if you don't need it.
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Opinionated: forbid root login through SSH.
+      PermitRootLogin = "no";
+      # Opinionated: use keys only.
+      # Remove if you want to SSH using passwords
+      PasswordAuthentication = false;
+    };
+  };
+
+  # ld for wsl for vscode server
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
+  };
+}
