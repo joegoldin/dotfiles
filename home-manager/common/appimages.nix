a2148da283e4e1717fe166c3bd084224bd4ed1c8
joegoldin <joe@joegold.in>
2025-05-10 17:01:54 -0700

    wip

---
+++ home-manager/common/appimages.nix
@@ -0,0 +1,12 @@
+{pkgs}: let
+  # extraterm = pkgs.appimageTools.wrapType2 {
+  #   pname = "extraterm";
+  #   version = "0.81.0";
+  #   src = pkgs.fetchurl {
+  #     url = "https://github.com/sedwards2009/extraterm/releases/download/v0.81.0/ExtratermQt-0.81.0.glibc2.34-x86_64.AppImage";
+  #     hash = "sha256-0xkIJL3c5FesUbUOb1LAdkjkmbapD+uEsfeoc88kDzU=";
+  #   };
+  # };
+in [ 
+  # extraterm
+]
