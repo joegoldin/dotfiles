39a61a7a918aae43e7259890edfeb20df5d1dafa
joegoldin <joe@joegold.in>
2025-05-24 06:55:32 +0000

    fix wsl build

--- home-manager/common/cargo.nix
+++ home-manager/common/cargo.nix
@@ -6,33 +6,33 @@
   packages = {
     litra = pkgs.rustPlatform.buildRustPackage rec {
       pname = "litra-rs";
-      version = "v2.2.0";
+      version = "ffc76804b4933585d10f76b6234c068ca84d009a";
 
       buildInputs = with pkgs; [openssl systemd];
       nativeBuildInputs = with pkgs; [pkg-config];
 
       src = pkgs.fetchFromGitHub {
-        owner = "timrogers";
+        owner = "joegoldin";
         repo = pname;
         rev = version;
         hash = "sha256-0BwtC2gFdt8rri5WGGdqMThPdax/UrZRfpCykWMydhA=";
       };
-      cargoHash = "sha256-4X/jASuXDLKuc3EIeSSnksLDI5CkJYNP/bkPi+24fdo=";
+      cargoHash = "sha256-0T2oq+f7KwNn2nZVEsFBDEt2sRHe/Loq4zVx6jT7/us=";
     };
     litra-autotoggle = pkgs.rustPlatform.buildRustPackage rec {
       pname = "litra-autotoggle";
-      version = "v0.6.1";
+      version = "391de640657235219d663941120a02f127473f56";
 
       buildInputs = with pkgs; [openssl systemd];
       nativeBuildInputs = with pkgs; [pkg-config];
 
       src = pkgs.fetchFromGitHub {
-        owner = "timrogers";
+        owner = "joegoldin";
         repo = pname;
         rev = version;
         hash = "sha256-CoP9t8uvErvP3sU51pfsjsY/xp/zXNVcgXP8WmONz60=";
       };
-      cargoHash = "sha256-hg65H6kALuHbRpiT6CZKP3aBTSLoN0xDEf5QBVfyrq8=";
+      cargoHash = "sha256-MCabivlj8ye8WKMFJ9oP5+J72D8Ib0xlYEOjLCUKjYg=";
     };
   };
 }
