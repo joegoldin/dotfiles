785f9433059d5acda20de121ede7c0a36b657f64
joegoldin <joe@joegold.in>
2025-06-22 12:25:25 -0700

    fix mac for os 26

--- environments/darwin/system.nix
+++ environments/darwin/system.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, username, ...}:
 ###################################################################################
 #
 #  macOS's System configuration
@@ -12,11 +12,13 @@
 {
   system = {
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
+    activationScripts.activateSettings.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
+
+    primaryUser = username;
 
     defaults = {
       # menuExtraClock.Show24Hour = true;  # show 24 hour clock
