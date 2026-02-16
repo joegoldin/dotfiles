6a35d37bcebd286b406c7271781ab59562ed8f35
joegoldin <joe@joegold.in>
2026-02-16 09:42:03 -0800

    fix: fish ai init

--- hosts/common/home/fish/default.nix
+++ hosts/common/home/fish/default.nix
@@ -19,7 +19,7 @@
     lib.hm.dag.entryAfter ["linkGeneration"] ''
       export PATH="${lib.makeBinPath [pkgs.python3 pkgs.grc pkgs.git pkgs.uv]}:$PATH"
       echo "Running fish-ai installation..."
-      run ${pkgs.fish}/bin/fish -c 'source ${config.xdg.configHome}/fish/conf.d/fish_ai.fish; _fish_ai_install'
+      run ${pkgs.fish}/bin/fish -c 'source ${config.xdg.configHome}/fish/conf.d/plugin-fish-ai.fish; _fish_ai_install'
     ''
   );
 }
