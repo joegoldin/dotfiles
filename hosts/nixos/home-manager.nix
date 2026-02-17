89da32b0be677df039f83a247826ed08de3f7484
joegoldin <joe@joegold.in>
2026-02-16 20:42:44 -0800

    persist plasma settings

--- hosts/nixos/home-manager.nix
+++ hosts/nixos/home-manager.nix
@@ -98,6 +98,7 @@
 
     # Shortcuts
     shortcuts = {
+      "ActivityManager"."switch-to-activity-8a44913d-258b-4faf-b84c-6815d74e5cf1" = [];
       "ActivityManager"."switch-to-activity-aa47102b-a33c-407f-8039-dbf5985eb3e6" = [];
       "KDE Keyboard Layout Switcher"."Switch to Last-Used Keyboard Layout" = "Meta+Alt+L";
       "KDE Keyboard Layout Switcher"."Switch to Next Keyboard Layout" = "Meta+Alt+K";
@@ -132,37 +133,6 @@
       "kwin"."ExposeClassCurrentDesktop" = [];
       "kwin"."Grid View" = "Meta+G";
       "kwin"."Increase Opacity" = "none,,Increase Opacity of Active Window by 5%";
-      "kwin"."KZones: Activate layout 1" = [];
-      "kwin"."KZones: Activate layout 2" = [];
-      "kwin"."KZones: Activate layout 3" = [];
-      "kwin"."KZones: Activate layout 4" = [];
-      "kwin"."KZones: Activate layout 5" = [];
-      "kwin"."KZones: Activate layout 6" = [];
-      "kwin"."KZones: Activate layout 7" = [];
-      "kwin"."KZones: Activate layout 8" = [];
-      "kwin"."KZones: Activate layout 9" = [];
-      "kwin"."KZones: Cycle layouts" = [];
-      "kwin"."KZones: Cycle layouts (reversed)" = [];
-      "kwin"."KZones: Move active window down" = [];
-      "kwin"."KZones: Move active window left" = [];
-      "kwin"."KZones: Move active window right" = [];
-      "kwin"."KZones: Move active window to next zone" = [];
-      "kwin"."KZones: Move active window to previous zone" = [];
-      "kwin"."KZones: Move active window to zone 1" = [];
-      "kwin"."KZones: Move active window to zone 2" = [];
-      "kwin"."KZones: Move active window to zone 3" = [];
-      "kwin"."KZones: Move active window to zone 4" = [];
-      "kwin"."KZones: Move active window to zone 5" = [];
-      "kwin"."KZones: Move active window to zone 6" = [];
-      "kwin"."KZones: Move active window to zone 7" = [];
-      "kwin"."KZones: Move active window to zone 8" = [];
-      "kwin"."KZones: Move active window to zone 9" = [];
-      "kwin"."KZones: Move active window up" = [];
-      "kwin"."KZones: Snap active window" = [];
-      "kwin"."KZones: Snap all windows" = [];
-      "kwin"."KZones: Switch to next window in current zone" = [];
-      "kwin"."KZones: Switch to previous window in current zone" = [];
-      "kwin"."KZones: Toggle zone overlay" = [];
       "kwin"."Kill Window" = "Meta+Ctrl+Esc";
       "kwin"."Move Tablet to Next Output" = [];
       "kwin"."MoveMouseToCenter" = "Meta+F6";
@@ -354,6 +324,14 @@
       "plasmashell"."switch to previous activity" = "none,,Switch to Previous Activity";
       "plasmashell"."toggle do not disturb" = "none,,Toggle do not disturb";
       "services/org.kde.dolphin.desktop"."_launch" = [];
+      "services/com.mitchellh.ghostty.desktop"."_launch" = [];
+      "kwin"."Window Custom Quick Tile Bottom" = [];
+      "kwin"."Window Custom Quick Tile Left" = [];
+      "kwin"."Window Custom Quick Tile Right" = [];
+      "kwin"."Window Custom Quick Tile Top" = [];
+      "kwin"."disableInputCapture" = "Meta+Shift+Esc";
+      "plasmashell"."Slideshow Wallpaper Next Image" = [];
+      "plasmashell"."edit_clipboard" = [];
     };
 
     # Custom hotkeys
@@ -386,6 +364,8 @@
       "baloofilerc"."General"."dbVersion" = 2;
       "baloofilerc"."General"."exclude filters" = "*~,*.part,*.o,*.la,*.lo,*.loT,*.moc,moc_*.cpp,qrc_*.cpp,ui_*.h,cmake_install.cmake,CMakeCache.txt,CTestTestfile.cmake,libtool,config.status,confdefs.h,autom4te,conftest,confstat,Makefile.am,*.gcode,.ninja_deps,.ninja_log,build.ninja,*.csproj,*.m4,*.rej,*.gmo,*.pc,*.omf,*.aux,*.tmp,*.po,*.vm*,*.nvram,*.rcore,*.swp,*.swap,lzo,litmain.sh,*.orig,.histfile.*,.xsession-errors*,*.map,*.so,*.a,*.db,*.qrc,*.ini,*.init,*.img,*.vdi,*.vbox*,vbox.log,*.qcow2,*.vmdk,*.vhd,*.vhdx,*.sql,*.sql.gz,*.ytdl,*.tfstate*,*.class,*.pyc,*.pyo,*.elc,*.qmlc,*.jsc,*.fastq,*.fq,*.gb,*.fasta,*.fna,*.gbff,*.faa,po,CVS,.svn,.git,_darcs,.bzr,.hg,CMakeFiles,CMakeTmp,CMakeTmpQmake,.moc,.obj,.pch,.uic,.npm,.yarn,.yarn-cache,__pycache__,node_modules,node_packages,nbproject,.terraform,.venv,venv,core-dumps,lost+found";
       "baloofilerc"."General"."exclude filters version" = 9;
+      "kactivitymanagerdrc"."activities"."8a44913d-258b-4faf-b84c-6815d74e5cf1" = "Default";
+      "kactivitymanagerdrc"."main"."currentActivity" = "8a44913d-258b-4faf-b84c-6815d74e5cf1";
       "dolphinrc"."DetailsMode"."PreviewSize" = 16;
       "dolphinrc"."General"."RememberOpenedTabs" = false;
       "dolphinrc"."IconsMode"."PreviewSize" = 48;
@@ -449,27 +429,32 @@
       "kwinrc"."Desktops"."Name_2" = "Alternate";
       "kwinrc"."Desktops"."Number" = 2;
       "kwinrc"."Desktops"."Rows" = 1;
-      "kwinrc"."Plugins"."kzonesEnabled" = false;
-      "kwinrc"."Script-kzones"."trackLayoutPerScreen" = true;
+      "kwinrc"."Plugins"."synchronizeskipswitcherEnabled" = true;
       "kwinrc"."Tiling"."padding" = 4;
-      "kwinrc"."Tiling/0bde4fed-54b1-5a65-a80d-e15f3cbd3f51"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b{\"width\":0.25},{\"width\":0.5},{\"width\":0.25}\x5d}";
-      "kwinrc"."Tiling/3dfcd882-3b53-5dad-9264-f6f30f55e708"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b{\"width\":0.25},{\"width\":0.5},{\"width\":0.25}\x5d}";
-      "kwinrc"."Tiling/5914c4f2-13ba-5ca4-8d56-407f168ebfef"."tiles" = "{\"layoutDirection\":\"floating\",\"tiles\":\x5b{\"height\":0.8583333333333333,\"width\":0.9494791666666667,\"x\":0.0171875,\"y\":0.0462962962962963}\x5d}";
-      "kwinrc"."Tiling/5dfbdf8b-b6bc-57d9-8bf5-c73b48822a98"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b{\"width\":0.25},{\"width\":0.5},{\"width\":0.25}\x5d}";
-      "kwinrc"."Tiling/87014f83-7840-573e-aa8c-2c4c0ff954c2"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b{\"width\":0.25},{\"width\":0.5},{\"width\":0.25}\x5d}";
-      "kwinrc"."Tiling/af452a7d-ff5a-5a61-a7d1-8663a64cd1ec"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b\x5d}";
-      "kwinrc"."Tiling/bfbeccc8-9101-5191-a12c-1c8669f3c4eb"."tiles" = "{\"layoutDirection\":\"floating\",\"tiles\":\x5b{\"height\":0.4526041666666645,\"width\":0.9611111111111077,\"x\":0.01759259259259259,\"y\":0.5104166666666689},{\"height\":0.46979166666667393,\"width\":0.9666666666666612,\"x\":0.016666666666666597,\"y\":0.02395833333333642}\x5d}";
-      "kwinrc"."Tiling/ce0b05ff-c107-52c0-97a5-422e8c49aef6"."tiles" = "{\"layoutDirection\":\"floating\",\"tiles\":\x5b{\"height\":0.8892361111111093,\"width\":0.8942382812499999,\"x\":0.05,\"y\":0.05}\x5d}";
-      "kwinrc"."Tiling/d371bb4f-2002-53ff-9df3-273219688491"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b{\"width\":0.25},{\"width\":0.5},{\"width\":0.25}\x5d}";
-      "kwinrc"."Tiling/e151800d-6cc9-5fed-9b34-1cad9484bacb"."tiles" = "{\"layoutDirection\":\"floating\",\"tiles\":\x5b{\"height\":0.949999999999999,\"width\":0.9479166666666676,\"x\":0.025520833333333333,\"y\":0.0027777777777777775}\x5d}";
-      "kwinrc"."Tiling/e6150747-0dca-5305-bd64-e7673cc0d170"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b{\"width\":0.25},{\"width\":0.5},{\"width\":0.25}\x5d}";
-      "kwinrc"."Tiling/f19ba19e-2ce1-5e23-ae4e-569a8e48ee92"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":\x5b{\"layoutDirection\":\"floating\",\"tiles\":\x5b{\"height\":0.8965277777777776,\"width\":0.7894531249999752,\"x\":0.07148437499999993,\"y\":0.050000000000000155},{\"height\":0.7597222222222202,\"width\":0.36015624999999607,\"x\":0.6312500000000021,\"y\":0.029861111111111085},{\"height\":0.8805555555555554,\"width\":0.45234375000000104,\"x\":0.016406250000003054,\"y\":0.020833333333333603}\x5d,\"width\":0.9999999999999984}\x5d}";
+      # Per-desktop per-screen tiling layouts (Desktop 1: Main)
+      "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/1f2c90e2-3a8a-4a2e-831e-02e10fc958cd"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":1}]}'';
+      "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/2fd71289-0069-4dd8-b8d9-85a14c65cd2e"."tiles" = ''{"layoutDirection":"floating","tiles":[{"height":0.959259259259256,"width":0.8739583333333356,"x":0.03437500000000005,"y":0.012037037037036995},{"height":0.9888888888888838,"width":0.44427083333334144,"x":0.0005208333333335274,"y":0.004629629629629532},{"height":0.9879629629629589,"width":0.4630208333333371,"x":0.4567708333333289,"y":0.005555555555555555}]}'';
+      "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/3f40670c-5b6f-4904-835d-62d17f2324f5"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.5},{"width":0.5}]}'';
+      "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/71cde4f2-86c2-44dc-9896-c4c025c5c5fb"."tiles" = ''{"layoutDirection":"floating","tiles":[{"height":0.7958333333333314,"width":0.8171875000000013,"x":0.08281249999999869,"y":0.09166666666666654},{"height":0.7562499999999982,"width":0.3812499999999967,"x":0.0191406249999989,"y":0.05972222222222237},{"height":0.8840277777777755,"width":0.394140624999995,"x":0.597656250000001,"y":0.04791666666666706}]}'';
+      "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/bd507e42-a7b4-4a0e-8871-ee8e19d10874"."tiles" = ''{"layoutDirection":"floating","tiles":[{"height":0.49322916666666655,"width":0.979629629629628,"x":0.011111111111110945,"y":0.010416666666666692},{"height":0.47447916666666745,"width":0.9703703703703689,"x":0.012962962962962671,"y":0.5156249999999998}]}'';
+      # Per-screen tiling layouts for screen 0d989076
+      "kwinrc"."Tiling/0d989076-4679-486f-b77a-d70ddebcf0b8/2fd71289-0069-4dd8-b8d9-85a14c65cd2e"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
+      "kwinrc"."Tiling/0d989076-4679-486f-b77a-d70ddebcf0b8/71cde4f2-86c2-44dc-9896-c4c025c5c5fb"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
+      "kwinrc"."Tiling/0d989076-4679-486f-b77a-d70ddebcf0b8/bd507e42-a7b4-4a0e-8871-ee8e19d10874"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
+
+      # Per-desktop per-screen tiling layouts (Desktop 2: Alternate)
+      "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/1f2c90e2-3a8a-4a2e-831e-02e10fc958cd"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
+      "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/2fd71289-0069-4dd8-b8d9-85a14c65cd2e"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
+      "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/3f40670c-5b6f-4904-835d-62d17f2324f5"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
+      "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/71cde4f2-86c2-44dc-9896-c4c025c5c5fb"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
+      "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/bd507e42-a7b4-4a0e-8871-ee8e19d10874"."tiles" = ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
       "kwinrc"."Xwayland"."Scale" = 1.25;
       "kwinrc"."org.kde.kdecoration2"."ButtonsOnLeft" = "XAI";
       "kwinrc"."org.kde.kdecoration2"."ButtonsOnRight" = "FSM";
       "kxkbrc"."Layout"."Options" = "caps:ctrl_modifier";
       "kxkbrc"."Layout"."ResetOldOptions" = true;
       "plasma-localerc"."Formats"."LANG" = "en_US.UTF-8";
+      "plasmanotifyrc"."Applications/com.mitchellh.ghostty"."Seen" = true;
       "plasmanotifyrc"."Applications/discord"."Seen" = true;
       "plasmanotifyrc"."Applications/firefox"."Seen" = true;
       "plasmanotifyrc"."Applications/slack"."Seen" = true;
@@ -483,6 +468,41 @@
       "dolphin/view_properties/global/.directory"."Dolphin"."SortOrder" = 1;
       "dolphin/view_properties/global/.directory"."Dolphin"."SortRole" = "modificationtime";
       "dolphin/view_properties/global/.directory"."Dolphin"."ViewMode" = 1;
+      # Kate anonymous session defaults
+      "kate/anonymous.katesession"."Kate Plugins"."bookmarksplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."cmaketoolsplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."compilerexplorer" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."eslintplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."externaltoolsplugin" = true;
+      "kate/anonymous.katesession"."Kate Plugins"."formatplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katebacktracebrowserplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katebuildplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katecloseexceptplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katecolorpickerplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katectagsplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katefilebrowserplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katefiletreeplugin" = true;
+      "kate/anonymous.katesession"."Kate Plugins"."kategdbplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."kategitblameplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katekonsoleplugin" = true;
+      "kate/anonymous.katesession"."Kate Plugins"."kateprojectplugin" = true;
+      "kate/anonymous.katesession"."Kate Plugins"."katereplicodeplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katesearchplugin" = true;
+      "kate/anonymous.katesession"."Kate Plugins"."katesnippetsplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katesqlplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katesymbolviewerplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katexmlcheckplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."katexmltoolsplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."keyboardmacrosplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."ktexteditorpreviewplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."latexcompletionplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."lspclientplugin" = true;
+      "kate/anonymous.katesession"."Kate Plugins"."openlinkplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."rainbowparens" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."rbqlplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."tabswitcherplugin" = true;
+      "kate/anonymous.katesession"."Kate Plugins"."templateplugin" = false;
+      "kate/anonymous.katesession"."Kate Plugins"."textfilterplugin" = true;
     };
 
     # Configure spectacle screenshots
