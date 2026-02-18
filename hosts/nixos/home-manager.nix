668d3ec146d49baba6d55a6680119d418f7f7a4f
joegoldin <joe@joegold.in>
2026-02-18 12:48:01 -0800

    chore: persist plasma configs

--- hosts/nixos/home-manager.nix
+++ hosts/nixos/home-manager.nix
@@ -88,7 +88,18 @@
           }
           "org.kde.plasma.marginsseparator"
           "org.kde.plasma.systemtray"
-          "org.kde.plasma.digitalclock"
+          {
+            digitalClock = {
+              date.format = {
+                custom = "";
+              };
+              time.showSeconds = "always";
+              font = {
+                family = "Noto Sans";
+                weight = 400;
+              };
+            };
+          }
         ];
       }
     ];
@@ -440,36 +451,44 @@
       "kdeglobals"."WM"."inactiveBlend" = "161,169,177";
       "kdeglobals"."WM"."inactiveForeground" = "161,169,177";
       "krunnerrc"."General"."historyBehavior" = "ImmediateCompletion";
+      "kscreenlockerrc"."Daemon"."Timeout" = 10;
       "kwalletrc"."Wallet"."First Use" = false;
       "kwalletrc"."Wallet"."Prompt on Open" = false;
       "kwinrc"."Desktops"."Id_1" = "003e92e5-0a93-4e80-9e46-abcefff2f6ed";
-      "kwinrc"."Desktops"."Id_2" = "9d26031a-0f2f-4fdf-9d21-14462065db3a";
       "kwinrc"."Desktops"."Name_1" = "Main";
-      "kwinrc"."Desktops"."Name_2" = "Alternate";
-      "kwinrc"."Desktops"."Number" = 2;
+      "kwinrc"."Desktops"."Number" = 1;
       "kwinrc"."Desktops"."Rows" = 1;
+      "kwinrc"."Plugins"."kzonesEnabled" = false;
       "kwinrc"."Plugins"."synchronizeskipswitcherEnabled" = true;
+      "kwinrc"."Script-kzones"."trackLayoutPerScreen" = true;
       "kwinrc"."Tiling"."padding" = 4;
-      # Per-desktop per-screen tiling layouts (Desktop 1: Main)
+      # Per-virtual-desktop, per-monitor tiling layouts
       "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/1f2c90e2-3a8a-4a2e-831e-02e10fc958cd"."tiles" =
         ''{"layoutDirection":"horizontal","tiles":[{"width":1}]}'';
       "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/2fd71289-0069-4dd8-b8d9-85a14c65cd2e"."tiles" =
         ''{"layoutDirection":"floating","tiles":[{"height":0.959259259259256,"width":0.8739583333333356,"x":0.03437500000000005,"y":0.012037037037036995},{"height":0.9888888888888838,"width":0.44427083333334144,"x":0.0005208333333335274,"y":0.004629629629629532},{"height":0.9879629629629589,"width":0.4630208333333371,"x":0.4567708333333289,"y":0.005555555555555555}]}'';
       "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/3f40670c-5b6f-4904-835d-62d17f2324f5"."tiles" =
-        ''{"layoutDirection":"horizontal","tiles":[{"width":0.5},{"width":0.5}]}'';
+        ''{"layoutDirection":"floating","tiles":[{"height":0.9990740740740734,"width":0.49479166666666985,"x":0,"y":0},{"height":0.991666666666667,"width":0.9380208333333211,"x":0.030208333333335988,"y":0},{"height":0.9999999999999997,"width":0.49895833333333195,"x":0.5010416666666622,"y":2.94469310047063e-16}]}'';
       "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/71cde4f2-86c2-44dc-9896-c4c025c5c5fb"."tiles" =
-        ''{"layoutDirection":"floating","tiles":[{"height":0.7958333333333314,"width":0.8171875000000013,"x":0.08281249999999869,"y":0.09166666666666654},{"height":0.7562499999999982,"width":0.3812499999999967,"x":0.0191406249999989,"y":0.05972222222222237},{"height":0.8840277777777755,"width":0.394140624999995,"x":0.597656250000001,"y":0.04791666666666706}]}'';
+        ''{"layoutDirection":"floating","tiles":[{"height":0.9048611111111109,"width":0.9281249999999986,"x":0.04296875,"y":0.026388888888888882},{"height":0.7909722222222203,"width":0.3812499999999967,"x":0.0191406249999989,"y":0.05972222222222237},{"height":0.9118055555555532,"width":0.394140624999995,"x":0.597656250000001,"y":0.04791666666666706}]}'';
       "kwinrc"."Tiling/003e92e5-0a93-4e80-9e46-abcefff2f6ed/bd507e42-a7b4-4a0e-8871-ee8e19d10874"."tiles" =
         ''{"layoutDirection":"floating","tiles":[{"height":0.49322916666666655,"width":0.979629629629628,"x":0.011111111111110945,"y":0.010416666666666692},{"height":0.47447916666666745,"width":0.9703703703703689,"x":0.012962962962962671,"y":0.5156249999999998}]}'';
-      # Per-screen tiling layouts for screen 0d989076
+      "kwinrc"."Tiling/0bde4fed-54b1-5a65-a80d-e15f3cbd3f51"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5b{"width":0.25},{"width":0.5},{"width":0.25}x5d}'';
       "kwinrc"."Tiling/0d989076-4679-486f-b77a-d70ddebcf0b8/2fd71289-0069-4dd8-b8d9-85a14c65cd2e"."tiles" =
         ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
       "kwinrc"."Tiling/0d989076-4679-486f-b77a-d70ddebcf0b8/71cde4f2-86c2-44dc-9896-c4c025c5c5fb"."tiles" =
         ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
       "kwinrc"."Tiling/0d989076-4679-486f-b77a-d70ddebcf0b8/bd507e42-a7b4-4a0e-8871-ee8e19d10874"."tiles" =
         ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
-
-      # Per-desktop per-screen tiling layouts (Desktop 2: Alternate)
+      "kwinrc"."Tiling/3dfcd882-3b53-5dad-9264-f6f30f55e708"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5b{"width":0.25},{"width":0.5},{"width":0.25}x5d}'';
+      "kwinrc"."Tiling/5914c4f2-13ba-5ca4-8d56-407f168ebfef"."tiles" =
+        ''{"layoutDirection":"floating","tiles":x5b{"height":0.8583333333333333,"width":0.9494791666666667,"x":0.0171875,"y":0.0462962962962963}x5d}'';
+      "kwinrc"."Tiling/5dfbdf8b-b6bc-57d9-8bf5-c73b48822a98"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5b{"width":0.25},{"width":0.5},{"width":0.25}x5d}'';
+      "kwinrc"."Tiling/87014f83-7840-573e-aa8c-2c4c0ff954c2"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5b{"width":0.25},{"width":0.5},{"width":0.25}x5d}'';
       "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/1f2c90e2-3a8a-4a2e-831e-02e10fc958cd"."tiles" =
         ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
       "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/2fd71289-0069-4dd8-b8d9-85a14c65cd2e"."tiles" =
@@ -480,7 +499,21 @@
         ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
       "kwinrc"."Tiling/9d26031a-0f2f-4fdf-9d21-14462065db3a/bd507e42-a7b4-4a0e-8871-ee8e19d10874"."tiles" =
         ''{"layoutDirection":"horizontal","tiles":[{"width":0.25},{"width":0.5},{"width":0.25}]}'';
-      "kwinrc"."Xwayland"."Scale" = 1.25;
+      "kwinrc"."Tiling/af452a7d-ff5a-5a61-a7d1-8663a64cd1ec"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5bx5d}'';
+      "kwinrc"."Tiling/bfbeccc8-9101-5191-a12c-1c8669f3c4eb"."tiles" =
+        ''{"layoutDirection":"floating","tiles":x5b{"height":0.4526041666666645,"width":0.9611111111111077,"x":0.01759259259259259,"y":0.5104166666666689},{"height":0.46979166666667393,"width":0.9666666666666612,"x":0.016666666666666597,"y":0.02395833333333642}x5d}'';
+      "kwinrc"."Tiling/ce0b05ff-c107-52c0-97a5-422e8c49aef6"."tiles" =
+        ''{"layoutDirection":"floating","tiles":x5b{"height":0.8892361111111093,"width":0.8942382812499999,"x":0.05,"y":0.05}x5d}'';
+      "kwinrc"."Tiling/d371bb4f-2002-53ff-9df3-273219688491"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5b{"width":0.25},{"width":0.5},{"width":0.25}x5d}'';
+      "kwinrc"."Tiling/e151800d-6cc9-5fed-9b34-1cad9484bacb"."tiles" =
+        ''{"layoutDirection":"floating","tiles":x5b{"height":0.949999999999999,"width":0.9479166666666676,"x":0.025520833333333333,"y":0.0027777777777777775}x5d}'';
+      "kwinrc"."Tiling/e6150747-0dca-5305-bd64-e7673cc0d170"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5b{"width":0.25},{"width":0.5},{"width":0.25}x5d}'';
+      "kwinrc"."Tiling/f19ba19e-2ce1-5e23-ae4e-569a8e48ee92"."tiles" =
+        ''{"layoutDirection":"horizontal","tiles":x5b{"layoutDirection":"floating","tiles":x5b{"height":0.8965277777777776,"width":0.7894531249999752,"x":0.07148437499999993,"y":0.050000000000000155},{"height":0.7597222222222202,"width":0.36015624999999607,"x":0.6312500000000021,"y":0.029861111111111085},{"height":0.8805555555555554,"width":0.45234375000000104,"x":0.016406250000003054,"y":0.020833333333333603}x5d,"width":0.9999999999999984}x5d}'';
+      "kwinrc"."Xwayland"."Scale" = 1;
       "kwinrc"."org.kde.kdecoration2"."ButtonsOnLeft" = "XAI";
       "kwinrc"."org.kde.kdecoration2"."ButtonsOnRight" = "FSM";
       "kxkbrc"."Layout"."Options" = "caps:ctrl_modifier";
