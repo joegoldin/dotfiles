080539fbfb08455fd316929a76ac1ed3d9e85a52
joegoldin <joe@joegold.in>
2026-03-01 22:19:10 -0800

    feat: add flags support to script builder, add --volume to sfx
    Extend the script builder with an optional `flags` field for --flag
    style options. Auto-generates usage, help text, and fish completions
    from flag definitions. Use it in sfx for a --volume option.

--- hosts/common/home/bin/scripts/sfx.nix
+++ hosts/common/home/bin/scripts/sfx.nix
@@ -9,7 +9,21 @@
       completions = "ls $HOME/dotfiles/assets/sfx/ 2>/dev/null | string replace -r '\\.ogg$' ''";
     }
   ];
+  flags = [
+    {
+      name = "--volume";
+      arg = "VOL";
+      desc = "Volume 0-1.0 (default 1.0)";
+    }
+  ];
   body = ''
-    pw-play "$HOME/dotfiles/assets/sfx/$argv[1].ogg" &
+    set -l vol 1.0
+    if set -l idx (contains -i -- --volume $argv)
+        set vol $argv[(math $idx + 1)]
+        set -e argv[$idx]
+        set -e argv[$idx]
+    end
+
+    pw-play --volume $vol "$HOME/dotfiles/assets/sfx/$argv[1].ogg" &
   '';
 }
