baae14b61db9f718d8feeb17c657534670373fdb
joegoldin <joe@joegold.in>
2026-03-10 19:08:11 -0700

    refactor: move all ML packages out of common python env
    Move datasets, scikit-learn, transformers, wandb, and mlx to
    host-specific python.nix files so servers don't build torch/triton/LLVM.

--- hosts/darwin/python.nix
+++ hosts/darwin/python.nix
@@ -1,8 +1,21 @@
-# ML packages for macOS (pre-built torch binaries)
+# ML packages for macOS (pre-built torch binaries + MLX)
+{ pkgs, lib, ... }:
+let
+  inherit (pkgs) unstable;
+  customPackages = import ../common/home/python/custom-pypi-packages.nix {
+    inherit pkgs lib;
+    pythonBase = unstable.python3;
+  };
+in
 {
   custom.python.extraPackages =
     ps: with ps; [
+      datasets
+      scikit-learn
       torch-bin
       torchvision-bin
+      transformers
+      wandb
+      customPackages.mlx
     ];
 }
