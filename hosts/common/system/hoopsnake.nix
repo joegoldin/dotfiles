2fb0319acf064230ab7c3e54e6e975ae5989c74c
joegoldin <joe@joegold.in>
2026-03-17 18:02:46 -0700

    feat: add shared hoopsnake module for remote LUKS unlock

---
+++ hosts/common/system/hoopsnake.nix
@@ -0,0 +1,40 @@
+# hosts/common/system/hoopsnake.nix
+# Remote LUKS unlock via SSH over Tailscale using hoopsnake
+{
+  config,
+  dotfiles-secrets,
+  ...
+}:
+{
+  # Decrypt hoopsnake secrets via agenix
+  age.secrets.hoopsnake-host-key = {
+    file = "${dotfiles-secrets}/hoopsnake-host-key.age";
+    mode = "0400";
+    owner = "root";
+  };
+  age.secrets.hoopsnake-ts-authkey = {
+    file = "${dotfiles-secrets}/hoopsnake-ts-authkey.age";
+    mode = "0400";
+    owner = "root";
+  };
+
+  boot.initrd.network = {
+    enable = true;
+
+    hoopsnake = {
+      enable = true;
+
+      ssh = {
+        authorizedKeysFile = builtins.toFile "hoopsnake-authorized-keys"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0vgzxNgZd51jZ3K/s64jltFRSyVLxjLPWM4Q6747Zw";
+        privateHostKey = config.age.secrets.hoopsnake-host-key.path;
+      };
+
+      tailscale = {
+        tags = [ "tag:hoopsnake" ];
+        environmentFile = config.age.secrets.hoopsnake-ts-authkey.path;
+        cleanup.deleteExisting = true;
+      };
+    };
+  };
+}
