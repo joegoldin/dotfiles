65939b242051ddbceec4389570da5eaf4dbe76cd
joegoldin <joe@joegold.in>
2026-03-31 00:31:24 -0700

    feat(steamdeck): add Steam Deck host configuration files

---
+++ hosts/steamdeck/configuration.nix
@@ -0,0 +1,110 @@
+# hosts/steamdeck/configuration.nix
+{
+  inputs,
+  commonOverlays,
+  lib,
+  config,
+  pkgs,
+  username,
+  hostname,
+  stateVersion,
+  agenix,
+  keys,
+  ...
+}:
+{
+  system.stateVersion = "${stateVersion}";
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  nixpkgs = {
+    overlays = commonOverlays;
+    config = {
+      allowUnfree = true;
+      experimental-features = "nix-command flakes";
+    };
+  };
+
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in
+    {
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        nix-path = config.nix.nixPath;
+        trusted-users = [ "${username}" ];
+        auto-optimise-store = true;
+        builders-use-substitutes = true;
+      };
+
+      gc = {
+        automatic = lib.mkDefault true;
+        options = lib.mkDefault "--delete-older-than 14d";
+      };
+
+      extraOptions = lib.optionalString (
+        config.nix.package == pkgs.nixVersions.stable
+      ) "experimental-features = nix-command flakes";
+
+      registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+      channel.enable = false;
+    };
+
+  time.timeZone = "America/Los_Angeles";
+
+  networking.networkmanager.enable = true;
+  networking.hostName = hostname;
+
+  users.users."${username}" = {
+    uid = 1000;
+    shell = pkgs.fish;
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      keys.${username}
+    ];
+    extraGroups = [
+      "wheel"
+      "audio"
+      "video"
+      "networkmanager"
+      "input"
+    ];
+  };
+
+  programs = {
+    fish.enable = true;
+    nix-ld.enable = true;
+    nh = {
+      enable = true;
+      flake = "/home/${username}/dotfiles";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
+    git
+    wget
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
+  };
+
+  services.tailscale = {
+    enable = true;
+    package = pkgs.unstable.tailscale;
+  };
+}
