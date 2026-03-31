631ebcf5d0aaf6bf65449b84ff913b326058cd57
joegoldin <joe@joegold.in>
2026-03-31 01:08:55 -0700

    feat(steamdeck): add vendor drivers, Decky Loader, ProtonGE, protontricks

--- hosts/steamdeck/jovian.nix
+++ hosts/steamdeck/jovian.nix
@@ -1,20 +1,79 @@
 # hosts/steamdeck/jovian.nix
-# Jovian NixOS: Steam Deck hardware, Game Mode, desktop switching
+# Jovian NixOS: Steam Deck hardware, Game Mode, desktop switching, Decky Loader
 {
+  config,
   pkgs,
   lib,
   username,
   dotfiles-assets,
   ...
 }:
+let
+  inherit (lib) attrValues makeSearchPathOutput;
+in
 {
+  # Steam Deck hardware
   jovian.devices.steamdeck.enable = true;
+  jovian.devices.steamdeck.enableVendorDrivers = true;
   jovian.hardware.has.amd.gpu = true;
-  jovian.steam.enable = true;
-  jovian.steam.autoStart = true;
-  jovian.steam.user = username;
-  jovian.steam.desktopSession = "plasma";
 
+  # Steam + gamescope session
+  jovian.steam = {
+    enable = true;
+    autoStart = true;
+    user = username;
+    desktopSession = "plasma";
+
+    environment = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+        makeSearchPathOutput "steamcompattool" ""
+          config.programs.steam.extraCompatPackages;
+    };
+  };
+
+  # Additional Steam config (merged with gaming.nix)
+  programs.steam = {
+    protontricks.enable = true;
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
+    extraPackages = attrValues {
+      inherit (pkgs) flatpak;
+      inherit (pkgs.kdePackages) breeze;
+    };
+  };
+
+  # Decky Loader
+  jovian.decky-loader = {
+    enable = true;
+    user = username;
+    stateDir = "/home/${username}/.local/share/decky";
+
+    extraPackages = attrValues {
+      inherit (pkgs)
+        gawk
+        curl
+        unzip
+        util-linux
+        gnugrep
+        readline
+        procps
+        pciutils
+        flatpak
+        libpulseaudio
+        ;
+    };
+
+    extraPythonPackages = p: with p; [ click ];
+  };
+
+  # Extra gaming packages
+  environment.systemPackages = with pkgs; [
+    steam-rom-manager
+    r2modman
+  ];
+
+  # Fonts
   fonts.packages =
     let
       customFonts = import ../common/system/fonts { inherit pkgs lib dotfiles-assets; };
