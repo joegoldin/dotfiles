5d5a94f9b0bfa17dc21fe0e6aa5f9a950b8e7cda
joegoldin <joe@joegold.in>
2026-03-31 08:55:26 -0700

    feat(steamdeck): enable attic client push and post-build hook

--- hosts/steamdeck/default.nix
+++ hosts/steamdeck/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ../common/system/attic.nix
+    ../common/system/attic-post-build-hook.nix
     ../common/system/gaming.nix
     ./configuration.nix
     ./hardware-configuration.nix
