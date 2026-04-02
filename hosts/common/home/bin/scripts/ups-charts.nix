0dbe2f88b701f90bb61fc8f2a070f24b689e30b8
joegoldin <joe@joegold.in>
2026-04-02 01:19:20 -0700

    feat: combine charts - charge/load, watts/VA, input/output voltage

--- hosts/common/home/bin/scripts/ups-charts.nix
+++ hosts/common/home/bin/scripts/ups-charts.nix
@@ -34,7 +34,7 @@
         except Exception:
             return 0.0
 
-    charge, load, watts, va, voltage = [], [], [], [], []
+    charge, load, watts, va, in_volt, out_volt = [], [], [], [], [], []
     times = []
     t0 = time.monotonic()
 
@@ -45,40 +45,37 @@
         load.append(query("ups.load"))
         watts.append(query("ups.realpower"))
         va.append(query("ups.power"))
-        voltage.append(query("input.voltage"))
+        in_volt.append(query("input.voltage"))
+        out_volt.append(query("output.voltage"))
         times.append(round(time.monotonic() - t0))
 
         if len(times) > maxpts:
-            for lst in (charge, load, watts, va, voltage, times):
+            for lst in (charge, load, watts, va, in_volt, out_volt, times):
                 del lst[0]
 
         plt.clf()
-        plt.subplots(2, 2)
+        plt.subplots(1, 3)
         plt.theme("dark")
 
         plt.subplot(1, 1)
-        plt.title("Charge")
+        plt.title("Charge / Load")
         plt.ylabel("%")
         plt.ylim(0, 100)
-        plt.plot(times, charge)
+        plt.plot(times, charge, label="Charge")
+        plt.plot(times, load, label="Load")
 
         plt.subplot(1, 2)
-        plt.title("Load")
-        plt.ylabel("%")
-        plt.ylim(0, 100)
-        plt.plot(times, load)
-
-        plt.subplot(2, 1)
         plt.title("Power")
         plt.ylim(0, 1500)
         plt.plot(times, watts, label="W")
         plt.plot(times, va, label="VA")
 
-        plt.subplot(2, 2)
+        plt.subplot(1, 3)
         plt.title("Voltage")
         plt.ylabel("V")
         plt.ylim(100, 140)
-        plt.plot(times, voltage)
+        plt.plot(times, in_volt, label="Input")
+        plt.plot(times, out_volt, label="Output")
 
         plt.show()
         time.sleep(interval)
