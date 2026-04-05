a32a608155faf1ecdc154b0d29c9ad8573c6037d
joegoldin <joe@joegold.in>
2026-04-04 21:26:57 -0700

    feat: add roomeqwizard and unstable.easyeffects to nixos desktop

--- hosts/nixos/packages.nix
+++ hosts/nixos/packages.nix
@@ -26,6 +26,7 @@ in
       unstable.darktable
       # unstable.davinci-resolve
       unstable.discord
+      unstable.easyeffects
       unstable.ffmpeg
       # hyprwhspr
       docker-buildx
@@ -47,6 +48,7 @@ in
       pulseaudio # pactl required by pulsemeeter's pmctl script
       unstable.pulsemeeter
       unstable.pulsemixer
+      roomeqwizard
       rclone
       reptyr
       rocmPackages.amdsmi
