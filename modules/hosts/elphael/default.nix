56855f52d503fb151f8163b84b722134031ee93a
Joe Goldin <joe@joegold.in>
2026-09-23 11:36:37 -0700

    Import the personal Pi profile and remove Standard Compute

--- modules/hosts/elphael/default.nix
+++ modules/hosts/elphael/default.nix
@@ -81,11 +81,6 @@ in
         mode = "0400";
         owner = meta.username;
       };
-      age.secrets.standardcompute_api_key = {
-        file = "${inputs.dotfiles-secrets}/standardcompute_api_key.age";
-        mode = "0400";
-        owner = meta.username;
-      };
       age.secrets.umans_api_key = {
         file = "${inputs.dotfiles-secrets}/umans_api_key.age";
         mode = "0400";
--- modules/hosts/torrent/default.nix
+++ modules/hosts/torrent/default.nix
@@ -69,11 +69,6 @@ in
         mode = "0400";
         owner = meta.username;
       };
-      age.secrets.standardcompute_api_key = {
-        file = "${inputs.dotfiles-secrets}/standardcompute_api_key.age";
-        mode = "0400";
-        owner = meta.username;
-      };
       age.secrets.attic-token = {
         file = "${inputs.dotfiles-secrets}/attic.token.age";
         mode = "0400";
